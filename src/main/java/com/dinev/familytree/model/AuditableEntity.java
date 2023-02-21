2ef44c11b4779db87fe81d0e6a624c7eb782c04c
Emil Dinev <emil.dinev@hotmail.co.uk>
2023-02-21 15:47:40 +0200

    create AuditableEntity.java

--- src/main/java/com/dinev/familytree/model/AuditableEntity.java
+++ src/main/java/com/dinev/familytree/model/AuditableEntity.java
@@ -3,6 +3,10 @@ package com.dinev.familytree.model;
 import java.io.Serial;
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import lombok.AllArgsConstructor;
@@ -10,16 +14,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.GenericGenerator;
 
 
 @Getter
 @Setter
 @SuperBuilder
 @MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuditableEntity implements Serializable{
 
     @Serial
     private static final long serialVersionUID = -3830187817678880528L;
 
-    
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "member_id")
+    private Integer id;
 }
